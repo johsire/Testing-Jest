08e92db17626443e48699c028cd2b474133ca8a0
Josiah Maina <josiah5115@gmail.com>
2018-10-18 10:47:48 -0600

    tests

--- logic.test.js
+++ logic.test.js
@@ -1,4 +1,15 @@
 
+const { add } = require('./logic');
+
+
+test('can add 5 + 5 to be 10', () => {
+ expect(add(5, 5)).toBe(10)
+});
+
 test('can add 2 + 2 to be 4', () => {
  expect(add(2, 2)).toBe(4)
+});
+
+test('if given a string throw an error', () => {
+ expect(add('Bob', 'Dan')).toThrowError()
 });
